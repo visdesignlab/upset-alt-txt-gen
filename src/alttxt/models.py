46c18cdb224503492b74b1980ecf130cce6a9615
Jake Wagoner <jakewagoneredu@gmail.com>
2024-03-07 12:19:01 -0700

    Remove all rawData dependency

--- src/alttxt/models.py
+++ src/alttxt/models.py
@@ -1,6 +1,7 @@
 from alttxt.enums import AggregateBy, SortBy, SortVisibleBy
 from pydantic import BaseModel
 
+
 class Subset(BaseModel):
     """
     This represents a single intersection between two sets,
@@ -8,22 +9,24 @@ class Subset(BaseModel):
     Any field changes/additions need to be reflected in the
     SubsetField enum in types.py.
     """
+
     name: str
-    size: int # size
-    dev: float # Deviation
-    degree: int # Set to -1 if parser fails to find degree
+    size: int  # size
+    dev: float  # Deviation
+    degree: int  # Set to -1 if parser fails to find degree
+
 
 class DataModel(BaseModel):
     """
     For holding data from the "rawData" and "processedData" fields
     of the JSON data file.
     """
-    membs: list # of frozenset[str]
-    count: list # of int
-    sets: list # of str
-    sizes: dict # str -> int
-    subsets: list # of Subset
-    all_subsets: list # of All Subsets
+
+    count: list  # of int
+    sets: list  # of str
+    sizes: dict  # str -> int
+    subsets: list  # of Subset
+    all_subsets: list  # of All Subsets
 
 
 class FilterModel(BaseModel):
@@ -39,19 +42,21 @@ class BookmarkedIntersectionModel(BaseModel):
 
 
 class PlotModel(BaseModel):
-    scatterplots: list # of float
-    histograms: list # of float
-    wordclouds: list # of float
+    scatterplots: list  # of float
+    histograms: list  # of float
+    wordclouds: list  # of float
+
 
 class MetaDataModel(BaseModel):
     description: str
     sets: str
     items: str
 
+
 class GrammarModel(BaseModel):
     # TODO: Uncomment these if added to the JSON export
-    #caption: str
-    #title: str
+    # caption: str
+    # title: str
     first_aggregate_by: AggregateBy
     second_aggregate_by: AggregateBy
     first_overlap_degree: int
@@ -59,10 +64,10 @@ class GrammarModel(BaseModel):
     sort_visible_by: SortVisibleBy
     sort_by: SortBy
     filters: FilterModel
-    collapsed: list # of str
-    visible_sets: list # of str
-    visible_atts: list # of str
-    visible_set_sizes: dict # str -> int
+    collapsed: list  # of str
+    visible_sets: list  # of str
+    visible_atts: list  # of str
+    visible_set_sizes: dict  # str -> int
     plots: PlotModel
     metaData: MetaDataModel
-    bookmarked_intersections: list # of BookmarkedIntersectionModel
+    bookmarked_intersections: list  # of BookmarkedIntersectionModel
